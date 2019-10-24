694aee1bbb0fc87cf16486d412339471ac6594db
antonnifo <antonnifo@live.com>
2019-10-24 18:01:28 +0300

    rm class methods

--- resources/users.py
+++ resources/users.py
@@ -1,52 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-
-class User(object):
-
-    def __init__(self, id_, username, password):
-        self.id       = id_
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls, username):
-
-        conn   = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-
-        q = "SELECT * FROM  users WHERE username=?"
-        
-        results = cursor.execute(q,(username,))
-        row     = results.fetchone()
-        
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        conn.close()
-
-        return user
-
-    @classmethod
-    def find_by_id(cls, id_):
-
-        conn   = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-
-        q = "SELECT * FROM  users WHERE id=?"
-        
-        results = cursor.execute(q,(id_,))
-        row     = results.fetchone()
-        
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        conn.close()
-
-        return user
+from  models.user import UserModel
 
 class UserRegister(Resource):
     TABLE_NAME = 'users'
@@ -67,7 +21,7 @@ class UserRegister(Resource):
 
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"message": "User with that username already exists."}, 400
 
         connection = sqlite3.connect('data.db')
