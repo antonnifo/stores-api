c8067bc001bd98b5f56de4a43ba0a351de06ef35
antonnifo <antonnifo@live.com>
2019-10-28 16:32:12 +0300

    fix commit session bug

--- models/user.py
+++ models/user.py
@@ -2,19 +2,18 @@ from db_config import db
 
 class UserModel(db.Model):
     __tablename__ = 'users'
-     
-    # id_ = db.Column(db.models.IntegerField(_("")))
+
     id_ = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password): 
         self.username = username
         self.password = password
     
     def save_to_db(self):
         db.session.add(self)
-        db.commit()
+        db.session.commit()
 
     @classmethod
     def find_by_username(cls, username):
